f11a06026eb1623f7bf59b5661856d3c77f4750c
Dominik Croci <dominikrcroci@gmail.com>
2023-10-16 22:02:00 -0700

    fixed bug in filtering

--- src/App.jsx
+++ src/App.jsx
@@ -51,6 +51,7 @@ export default function App() {
         isDarkMode={isDarkMode}
         countries={countries}
         visibleCountries={searchResultCountries}
+        resultCount={searchResultCount}
       />
       <MoreResultsButton
         count={searchResultCount}
--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -9,55 +9,63 @@ export default function CountryList({
   searchValue,
 }) {
   const [countryElements, setCountryElements] = useState([]);
-  useEffect(() => {
-    const filteredCountries = countries.filter((country) => {
-      if (filterBy === 'africa') {
-        return country.region === 'Africa';
-      } else if (filterBy === 'asia') {
-        return country.region === 'Asia';
-      } else if (filterBy === 'europe') {
-        return country.region === 'Europe';
-      } else if (filterBy === 'oceania') {
-        return country.region === 'Oceania';
-      } else if (filterBy === 'americas') {
-        return country.region === 'Americas';
-      } else {
-        return true;
-      }
-    });
 
+  useEffect(() => {
+    // Filter countries based on the selected region, or show all countries if no specific region is selected
+    const filteredCountries =
+      filterBy === 'filter'
+        ? countries
+        : countries.filter((country) => {
+            if (filterBy === 'africa') {
+              return country.region === 'Africa';
+            } else if (filterBy === 'asia') {
+              return country.region === 'Asia';
+            } else if (filterBy === 'europe') {
+              return country.region === 'Europe';
+            } else if (filterBy === 'oceania') {
+              return country.region === 'Oceania';
+            } else if (filterBy === 'americas') {
+              return country.region === 'Americas';
+            } else {
+              return true;
+            }
+          });
+
+    // Further filter based on searchValue if present
     const filteredBySearch = searchValue
       ? filteredCountries.filter((country) =>
           country.name.common.toLowerCase().includes(searchValue.toLowerCase())
         )
-      : visibleCountries;
+      : filteredCountries;
 
-    const countryElements = filteredBySearch.map((country) => (
-      <div
-        key={country.name.common}
-        className={`${
-          isDarkMode ? 'bg-gray-800 text-white' : 'bg-white text-black'
-        } flex flex-col justify-between hover:scale-105`}
-      >
-        <img src={country.flags.png} alt="" className="w-full h-52" />
-        <h3 className="px-4 py-4 text-xl">
-          <span className="font-bold">{country.name.common}</span>
-        </h3>
-        <p className="px-4 p-2">
-          <span className="font-bold">Population:</span> {country.population}
-        </p>
-        <p className="px-4 pb-2">
-          <span className="font-bold">Region:</span> {country.region}
-        </p>
-        <p className="px-4 pb-2">
-          <span className="font-bold">Capital: </span>
-          {country.capital}
-        </p>
-      </div>
-    ));
-
-    setCountryElements(countryElements);
-  }, [countries, filterBy, isDarkMode, searchValue, visibleCountries]);
+    // Set the filtered countries to state
+    setCountryElements(
+      filteredBySearch.map((country) => (
+        <div
+          key={country.name.common}
+          className={`${
+            isDarkMode ? 'bg-gray-800 text-white' : 'bg-white text-black'
+          } flex flex-col justify-between hover:scale-105`}
+        >
+          <img src={country.flags.png} alt="" className="w-full h-52" />
+          <h3 className="px-4 py-4 text-xl">
+            <span className="font-bold">{country.name.common}</span>
+          </h3>
+          <p className="px-4 p-2">
+            <span className="font-bold">Population:</span>{' '}
+            {country.population.toLocaleString()}
+          </p>
+          <p className="px-4 pb-2">
+            <span className="font-bold">Region:</span> {country.region}
+          </p>
+          <p className="px-4 pb-2">
+            <span className="font-bold">Capital: </span>
+            {country.capital}
+          </p>
+        </div>
+      ))
+    );
+  }, [countries, filterBy, searchValue, isDarkMode]);
 
   return (
     <div className="grid md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-4 w-11/12 mx-auto pb-8">
@@ -65,7 +73,7 @@ export default function CountryList({
         countryElements
       ) : (
         <p
-          className={`col-start-1 col-end-5 mx-auto ${
+          className={`col-start-1 col-end-6 mx-auto w-full text-center ${
             isDarkMode ? ' text-white' : ' text-black'
           }`}
         >
